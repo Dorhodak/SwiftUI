c0b967bf7ecaffe0a9811e0ccf43a6e34d5ccf54
Dorhodak <dorhodak@icloud.com>
2026-03-17 02:31:36 +0700

    Solve One Button Conditional Challenge

---
+++ Challenges/One-Buttun-Message-Toggle/One-Buttun-Message-Toggle/One_Buttun_Message_ToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  One_Buttun_Message_ToggleApp.swift
+//  One-Buttun-Message-Toggle
+//
+//  Created by Vitaliy on 17.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct One_Buttun_Message_ToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
